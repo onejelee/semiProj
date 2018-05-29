e6f2d1f31bf508076a852afb722ae1437ccd4e35
wheretanpopo <36845403+wheretanpopo@users.noreply.github.com>
2018-05-29 20:01:44 +0900

    order vo

--- flowerRoadProj/src/com/fr/jsp/order/model/vo/Order.java
+++ flowerRoadProj/src/com/fr/jsp/order/model/vo/Order.java
@@ -1,5 +1,16 @@
 package com.fr.jsp.order.model.vo;
 
-public class Order {
+import java.util.Date;
 
+public class Order {
+	// 변수
+	   private String order_num;
+	   private String member_num;
+	   private String product_num;
+	   private Date ordered_date;
+	   private Date reservation_date;
+	   private String receiver_name;
+	   private String receiver_address;
+	   private String receiver_phone;
+	   private String order_state_code;
 }
